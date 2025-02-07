ae779043eb4fe0c7d1a789b6139b2786290c4d39
CarolinaLujan1994 <carolinaanahi.lujan@estudiantes.unahur.edu.ar>
2025-02-07 17:56:47 -0300

    muestra de datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+//import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'lujancarolina-cv';
+export class AppComponent implements OnInit {
+  title = 'Luján, Carolina';
+  public persona: any
+
+
+  ngOnInit() {
+    console.log('Componente inicializado')
+    this.datosSobreMi()
+    console.log(this.persona)
+  }
+
+  datosSobreMi() {
+    this.persona = {
+      nombre: 'Carolina',
+      descripcion: 'jasgdasjkldhajdhalkda',
+      foto: ''
+    }
+  }
 }
